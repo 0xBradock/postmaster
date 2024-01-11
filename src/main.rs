94998c12ea5d0f21174dc00e346a1586d3f3f282
0xBradock <0xbradock@proton.me>
2024-01-11 23:01:58 +0100

    feat: health check with log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(health_check))
+        .bind(("127.0.0.1", 5000))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
 }
