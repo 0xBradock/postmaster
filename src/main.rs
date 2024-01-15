09f58976133c23dabdb32519bdde75ab427e6239
0xBradock <0xbradock@proton.me>
2024-01-15 21:52:24 +0100

    feat: add health check route

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
-use env_logger::Env;
+use postmaster::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-
-    HttpServer::new(|| App::new().wrap(Logger::default()).service(health_check))
-        .bind(("127.0.0.1", 5000))?
-        .run()
-        .await
-}
-
-#[get("/")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
