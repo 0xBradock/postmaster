70f9b0b772c547a2d4fac58e7862cbc4a7a0418e
0xBradock <0xbradock@proton.me>
2024-01-25 22:03:16 +0100

    feat: add subscription route

--- src/startup.rs
+++ src/startup.rs
@@ -2,10 +2,10 @@ use actix_web::dev::Server;
 use actix_web::{App, HttpServer};
 use std::net::TcpListener;
 
-use crate::routes::health;
+use crate::routes::{health, subscriptions::subscriptions};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health))
+    let server = HttpServer::new(|| App::new().service(health).service(subscriptions))
         .listen(listener)?
         .run();
     Ok(server)
